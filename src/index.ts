3cd782a90265c993e1f2c2cd504147582c81f32d
StarkPrince <games.princeraj@gmail.com>
2023-05-09 18:37:25 +0530

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+import { User } from "./models/User";
+
+const user = new User({ id: 1 });
+
+user.set({ name: "new name", age: 999 });
+user.save();
+
+console.log(user.get("name"));
